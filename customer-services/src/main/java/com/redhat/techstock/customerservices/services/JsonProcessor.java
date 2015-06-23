08441de91b9be7327c91a45482969aac3ef780a4
Costas Sterpis <costas.sterpis@gmail.com>
2015-06-23 14:22:25 +0100

    reverting change

--- customer-services/src/main/java/com/redhat/techstock/customerservices/services/JsonProcessor.java
+++ customer-services/src/main/java/com/redhat/techstock/customerservices/services/JsonProcessor.java
@@ -16,10 +16,6 @@ public class JsonProcessor implements Processor {
 	public void process(Exchange exchange) throws Exception {
 		Gson gson = new Gson();
 		List list = exchange.getIn().getBody(List.class);
-		if (list.size() == 1){
-			exchange.getOut().setBody(gson.toJson(list.get(0)));
-		}else{
-			exchange.getOut().setBody(gson.toJson(list));
-		}
+		exchange.getOut().setBody(gson.toJson(list));
 	}
 }
